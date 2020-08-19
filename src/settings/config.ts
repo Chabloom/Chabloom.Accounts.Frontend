3182cbe9838cb18f9c44de6a371b092d635a8d17
Matthew Casey <mdcasey@chabloom.com>
2020-08-18 22:42:59 -0400

    Change config to typescript file

---
+++ src/settings/config.ts
@@ -0,0 +1,6 @@
+export const ApplicationConfig = {
+    "name": "chabloom-accounts",
+    "displayName": "Chabloom Accounts",
+    "apiPublicAddress": "API_PUBLIC_ADDRESS",
+    "jwtPublicAddress": "JWT_PUBLIC_ADDRESS"
+}
