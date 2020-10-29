ebd6bb8af3db606c04f625c848bd05ca445d0938
Matthew Casey <mdcasey@chabloom.com>
2020-10-28 22:42:17 -0400

    Add application insights

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,29 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Grid } from "@material-ui/core";
 
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
+import { ReactPlugin } from "@microsoft/applicationinsights-react-js";
+
+import { createBrowserHistory } from "history";
+
+import { AppInsightsInstrumentationKey } from "./settings/config";
+
 import { Error, Register, SignIn, SignOut } from "./components";
 
 import "./App.scss";
+
+const browserHistory = createBrowserHistory({ basename: "" });
+const reactPlugin = new ReactPlugin();
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: AppInsightsInstrumentationKey,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+  },
+});
+appInsights.loadAppInsights();
 
 export const App: React.FC = () => {
   return (
--- src/Config-Environment.ps1
+++ src/Config-Environment.ps1
@@ -3,4 +3,5 @@ $dir = Split-Path $scriptpath
 Push-Location $dir
 ((Get-Content -Path settings/config.ts -Raw) -replace 'API_PUBLIC_ADDRESS',$env:API_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'JWT_PUBLIC_ADDRESS',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
+((Get-Content -Path settings/config.ts -Raw) -replace 'APPINSIGHTS_INSTRUMENTATIONKEY',$env:APPINSIGHTS_INSTRUMENTATIONKEY) | Set-Content -Path settings/config.ts
 Pop-Location
