cde27f1fe85e0fe8dad54ae736934e4d26a96408
Matthew Casey <mdcasey@chabloom.com>
2020-11-10 10:05:10 -0500

    Move environment config script to scripts directory

--- scripts/Config-Environment.ps1
+++ scripts/Config-Environment.ps1
@@ -1,6 +1,6 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
-Push-Location $dir
+Push-Location $dir/../src/
 ((Get-Content -Path settings/config.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:API_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path settings/config.ts
 ((Get-Content -Path settings/config.ts -Raw) -replace 'APPINSIGHTS_INSTRUMENTATIONKEY',$env:APPINSIGHTS_INSTRUMENTATIONKEY) | Set-Content -Path settings/config.ts
