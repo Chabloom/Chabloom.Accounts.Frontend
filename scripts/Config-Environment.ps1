cbe35b91085862f4e125ea9c1e6cb729580ccb0b
Matthew Casey <mdcasey@chabloom.com>
2021-02-03 21:47:27 -0500

    Update settings

--- scripts/Config-Environment.ps1
+++ scripts/Config-Environment.ps1
@@ -1,7 +1,8 @@
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 Push-Location $dir/../src/
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:API_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
-((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:JWT_PUBLIC_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'http://localhost:3000',$env:ACCOUNTS_FRONTEND_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:ACCOUNTS_BACKEND_ADDRESS) | Set-Content -Path types/settings.ts
+((Get-Content -Path types/settings.ts -Raw) -replace 'https://accounts-api-test.chabloom.com',$env:ACCOUNTS_BACKEND_ADDRESS) | Set-Content -Path types/settings.ts
 ((Get-Content -Path types/settings.ts -Raw) -replace 'APPINSIGHTS_INSTRUMENTATIONKEY',$env:APPINSIGHTS_INSTRUMENTATIONKEY) | Set-Content -Path types/settings.ts
 Pop-Location
--- src/types/settings.ts
+++ src/types/settings.ts
@@ -1,8 +1,22 @@
+import { UserManagerSettings } from "oidc-client";
+
 export const ApplicationConfig = {
   name: "chabloom-accounts",
   displayName: "Chabloom Accounts",
-  apiPublicAddress: "https://accounts-api-test.chabloom.com",
-  jwtPublicAddress: "https://accounts-api-test.chabloom.com",
+  frontendPublicAddress: "http://localhost:3000",
+  backendPublicAddress: "https://accounts-api-test.chabloom.com",
+  accountsBackendPublicAddress: "https://accounts-api-test.chabloom.com",
 };
 
 export const AppInsightsInstrumentationKey = "APPINSIGHTS_INSTRUMENTATIONKEY";
+
+export const OidcSettings: UserManagerSettings = {
+  authority: ApplicationConfig.accountsBackendPublicAddress,
+  client_id: "Chabloom.Accounts.Frontend",
+  redirect_uri: `${ApplicationConfig.frontendPublicAddress}/signin-oidc`,
+  post_logout_redirect_uri: `${ApplicationConfig.frontendPublicAddress}/signout-oidc`,
+  response_type: "code",
+  scope: "openid profile Chabloom.Accounts",
+  filterProtocolClaims: true,
+  loadUserInfo: true,
+};
