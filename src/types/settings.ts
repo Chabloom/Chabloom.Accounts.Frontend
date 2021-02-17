7ff90ba80eb459eb0e011399d756092c62bc217c
Matthew Casey <mdcasey@chabloom.com>
2021-02-17 11:37:23 -0500

    Update settings URLs

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -3,9 +3,9 @@ import { UserManagerSettings } from "oidc-client";
 export const ApplicationConfig = {
   name: "chabloom-accounts",
   displayName: "Chabloom Accounts",
-  frontendPublicAddress: "http://localhost:3000",
-  backendPublicAddress: "https://accounts-api-test.chabloom.com",
-  accountsBackendPublicAddress: "https://accounts-api-test.chabloom.com",
+  frontendPublicAddress: "http://accounts-dev-1.chabloom.com",
+  backendPublicAddress: "https://accounts-api-dev-1.chabloom.com",
+  accountsBackendPublicAddress: "https://accounts-api-dev-1.chabloom.com",
 };
 
 export const AppInsightsInstrumentationKey = "APPINSIGHTS_INSTRUMENTATIONKEY";
@@ -16,7 +16,7 @@ export const OidcSettings: UserManagerSettings = {
   redirect_uri: `${ApplicationConfig.frontendPublicAddress}/signin-oidc`,
   post_logout_redirect_uri: `${ApplicationConfig.frontendPublicAddress}/signout-oidc`,
   response_type: "code",
-  scope: "openid profile Chabloom.Accounts",
+  scope: "openid profile Chabloom.Accounts.Backend",
   filterProtocolClaims: true,
   loadUserInfo: true,
 };
