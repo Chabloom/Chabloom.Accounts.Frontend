5398c918d342a701c102fad83aec1ee0c32c3720
Matthew Casey <mdcasey@chabloom.com>
2021-07-18 20:10:33 -0400

    Update dependencies

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, createStyles, FormGroup, Grid, Paper, TextField, Theme } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Button, FormGroup, Grid, Paper, TextField, Theme } from "@material-ui/core";
+import { makeStyles, createStyles } from "@material-ui/styles";
 
 import { AccountsApi, AccountViewModel } from "../../api";
 import { useAppContext } from "../../AppContext";
